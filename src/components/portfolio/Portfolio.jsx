0cadfd1f6c4a1f7dfd8ec9b322c8c8536606d065
Your Name <you@example.com>
2025-05-05 23:24:30 +0300

    Fix self-import in Portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './portfolio.module.css';
+import style from './Portfolio.module.css';
 import img1 from '../../assets/cabin.png';
 import img2 from '../../assets/cake.png';
 import img3 from '../../assets/circus.png';
@@ -17,8 +17,8 @@ export default function Portfolio() {
         <div className="row g-4">
           {images.map((img, index) => (
             <div className="col-md-4" key={index}>
-              <div className="bg-light rounded p-3 d-flex justify-content-center align-items-center" >
-                <img src={img} alt={`portfolio-${index}`} className={`${style.image}`}/>
+              <div className="bg-light rounded p-3 d-flex justify-content-center align-items-center">
+                <img src={img} alt={`portfolio-${index}`} className={`${style.image}`} />
               </div>
             </div>
           ))}
